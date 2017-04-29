dc4d1fe84d5af50777db23131ec3892124cfd1e1
FindHao <find@findspace.name>
2017-04-29 16:45:24 +0800

    窗口位置移动到鼠标旁边。关闭窗口按钮事件拦截为hide

--- entry.py
+++ entry.py
@@ -1,8 +1,9 @@
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QApplication, QDesktopWidget
 from PyQt5 import QtGui, QtCore
 import PyQt5.uic
 from a_cat import Query
 import sys
+
 (class_ui, class_basic_class) = PyQt5.uic.loadUiType('widget.ui')
 print(class_basic_class)
 
@@ -13,7 +14,6 @@ class MainFrame(class_basic_class, class_ui):
         # 使用qt自带的监听系统剪贴板的功能
         self.clipboard = QtGui.QGuiApplication.clipboard()
         self.setupUi(self)
-
         self.connects()
         # 这里还没太理解，主要是解决了focusoutevent事件不响应的bug
         self.setFocusPolicy(QtCore.Qt.StrongFocus)
@@ -28,6 +28,21 @@ class MainFrame(class_basic_class, class_ui):
 
     def refresh_window(self):
         """更新翻译的显示"""
+        # 设置位置和大小
+        self.setFixedSize(400, 300)
+        cur = QtGui.QCursor.pos()
+        x = cur.x() + 20
+        y = cur.y() + 20
+        # 如果超出了屏幕边界，便显示在里面
+        window_h = QDesktopWidget().screenGeometry().height()
+        window_w = QDesktopWidget().screenGeometry().width()
+        if x + 400 > window_h or y + 300 > window_w:
+            x -= 20 + 400
+            y -= 20 + 300
+        self.move(x, y)
+
+        self.setWindowTitle("search for: %s" % self.clipboard.text(QtGui.QClipboard.Selection))
+        # todo: 如果没有显示结果，需要提示
         voice_text = ''
         for x in self.query.word.voices:
             for key in x:
@@ -37,7 +52,6 @@ class MainFrame(class_basic_class, class_ui):
         base_info = ''
         for x in self.query.word.props:
             base_info += x + self.query.word.props[x] + '\n'
-        print(base_info)
         self.base_infor_label.setText(base_info)
 
     def selection_changed(self):
@@ -59,6 +73,11 @@ class MainFrame(class_basic_class, class_ui):
 
     def keyPressEvent(self, e):
         pass
+
+    def closeEvent(self, e):
+        """隐藏窗口到后台"""
+        self.hide()
+        e.ignore()
 
 
 if __name__ == '__main__':
